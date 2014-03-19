087509cffd1c1decab3e104fe161b151a7a845da
Austin Coleman <boxhockeyboy@gmail.com>
2014-03-18 23:00:10 -0700

    First commit of my Android Application

---
+++ src/com/example/application/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.application;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.annotation.TargetApi;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    // Inflate the menu items for use in the action bar
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main, menu);
+	    return super.onCreateOptionsMenu(menu);
+	}
+	
+	
+	public void sendMessage(View view){
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+	    EditText editText = (EditText) findViewById(R.id.edit_message);
+	    String message = editText.getText().toString();
+	    intent.putExtra(EXTRA_MESSAGE, message);
+	    startActivity(intent);
+	}
+	
+
+}
